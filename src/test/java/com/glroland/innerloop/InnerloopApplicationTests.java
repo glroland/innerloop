a42043576872bfcfad5a7ee3df45ea552a27859d
Lee Roland <glroland@hotmail.com>
2022-02-13 10:52:46 -0500

    initial revision

---
+++ src/test/java/com/glroland/innerloop/InnerloopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glroland.innerloop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InnerloopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
